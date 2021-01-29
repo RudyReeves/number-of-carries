3b73c3ec16b4b6cefd1a4eeed891147cc7eaf740
e r <reeves.rudy@gmail.com>
2021-01-29 13:55:49 -0500

    Clarify problem statement

--- numberOfCarries.js
+++ numberOfCarries.js
@@ -2,7 +2,7 @@
  * Problem Statement:
  * Given two numbers `n1` and `n2`, write a function that returns 
  * the number of carry operations required to add them digit-by-digit,
- * similar to pen-and-paper arithmetic with a number being carried as
+ * similar to pen-and-paper arithmetic with a one being carried as
  * the digits are added column-wise right-to-left, like this:
  * 
  *      49
@@ -10,7 +10,10 @@
  *    ----
  *     104
  * 
+ * The right-most column is 9+5=14, which two digits so we carry the one.
+ * The left-most column is 4+5=9, but receives the carried one.
  * One carry happened, so `f(49, 55)` is `1`.
+ * More tests are below.
  */
 
  // Here's my solution in JavaScript which is O(n):
