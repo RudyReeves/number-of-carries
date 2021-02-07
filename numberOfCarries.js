20a0dfbba4760b6e6b0a1aa8d23baff827c4c26b
e r <reeves.rudy@gmail.com>
2021-02-07 13:24:47 -0500

    Simplify for loop in code

--- numberOfCarries.js
+++ numberOfCarries.js
@@ -28,26 +28,20 @@
   * `f(n1, n2)` takes two integers and returns the integer number
   * of carries required to add `n1` and `n2`.
   */
-const f = (n1, n2) => {
+ const numberOfCarryOperations = (n1, n2) => {
     const digits1 = getDigits(n1).reverse();
     const digits2 = getDigits(n2).reverse();
-    const maxLength = Math.max(digits1.length, digits2.length);
-    const minLength = Math.min(digits1.length, digits2.length);
     let carry = 0;
-    let i = 0;
-    for (; i < minLength; i++) {
-        const e1 = digits1[i] || 0;
-        const e2 = digits2[i] || 0;
-        if (e1 + e2 >= 10) { // Check if this pair sums to two digits
+    const maxLength = Math.max(digits1.length, digits2.length);
+    for (let i = 0; i < maxLength; i++) {
+        const d1 = digits1[i] || 0;
+        const d2 = digits2[i] || 0;
+        if (
+            (d1 + d2 >= 10)
+            || (d1 === 9 && carry !== 0)
+            || (d2 === 9 && carry !== 0)
+        ) {
             carry++;
-        }
-    }
-    for (; i < maxLength; i++) {
-        const e = digits1.length > digits2.length ? digits1[i] : digits2[i];
-        if (e === 9) {
-            carry++;
-        } else {
-            break;
         }
     }
     return carry;
@@ -57,10 +51,13 @@ const getDigits = (n) => ('' + n).split('').map(Number);
 
 // Tests:
 
-console.log(f(55, 49)); // pass (1)
-console.log(f(50, 49)); // pass (0)
-console.log(f(12345, 4)); // pass (0)
-console.log(f(31, 24)); // pass (0)
-console.log(f(56799, 5)); // pass (2)
-console.log(f(6, 99999)); // pass (5)
-console.log(f(66, 99999)); // pass (5)
+console.log(numberOfCarryOperations(44, 56)); // pass (1)
+console.log(numberOfCarryOperations(9999, 1)); // pass (4)
+console.log(numberOfCarryOperations(1, 9999)); // pass (1)
+console.log(numberOfCarryOperations(55, 49)); // pass (1)
+console.log(numberOfCarryOperations(50, 49)); // pass (0)
+console.log(numberOfCarryOperations(12345, 4)); // pass (0)
+console.log(numberOfCarryOperations(31, 24)); // pass (0)
+console.log(numberOfCarryOperations(56799, 5)); // pass (2)
+console.log(numberOfCarryOperations(6, 99999)); // pass (5)
+console.log(numberOfCarryOperations(66, 99999)); // pass (5)
